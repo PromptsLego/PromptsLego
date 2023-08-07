8d2b46bc0f15d6554061b0ed0f9a3a8562e9199c
Shadow Dream <1820636951@qq.com>
2023-08-07 13:31:47 +0800

    Fixed Overflow of the central area

--- src/components/centerContent/CenterContent.tsx
+++ src/components/centerContent/CenterContent.tsx
@@ -5,51 +5,62 @@ import { useContext } from "react";
 import ContentContext from "../../contexts/ContentContext";
 import FrozeButton from "./FrozeButton";
 
-interface CenterContentProps {}
+interface CenterContentProps { }
 
-const CenterContent: React.FC<CenterContentProps> = ({}) => {
+const CenterContent: React.FC<CenterContentProps> = ({ }) => {
   const { current, SetCurrent } = useContext(ContentContext);
 
   return (
     <Content
       style={{
+        position: "relative",
         backgroundColor: "white",
         display: "flex",
         flexDirection: "column",
         flexWrap: "wrap",
         paddingLeft: "50px",
+        paddingRight: "50px",
         // justifyContent: "space-between",
       }}
     >
       <div style={{ alignSelf: "flex-end", marginRight: "10px" }}>
         <FrozeButton></FrozeButton>
       </div>
+      <div style={{
+        position: "absolute",
+        left:"10%",
+        right:"10%",
+        top:"0",
+        bottom:"10%",
+        overflowY:"auto"
+      }}>
+        {current.map((item) => {
+          return (
+            <>
+              {item.children.length === 0 ? (
+                <></>
+              ) : (
+                <Label text={item.category} />
+              )}
+              <div style={{ display: "flex", flexWrap: "wrap" }}>
+                {item.children.map((child) => {
+                  return (
+                    <Lego
+                      keyWord={child.keyWord}
+                      detail={child.detail}
+                      useTime={child.useTime}
+                      color={child.color}
+                      legoType="lego"
+                      varNum={child.varNum}
+                    />
+                  );
+                })}
+              </div>
+            </>
+          );
+        })}
+      </div>
 
-      {current.map((item) => {
-        return (
-          <>
-            {item.children.length === 0 ? (
-              <></>
-            ) : (
-              <Label text={item.category} />
-            )}
-            <div style={{ display: "flex", flexWrap: "wrap" }}>
-              {item.children.map((child) => {
-                return (
-                  <Lego
-                    keyWord={child.keyWord}
-                    detail={child.detail}
-                    useTime={child.useTime}
-                    color={child.color}
-                    legoType="lego"
-                    varNum={child.varNum}
-                  />
-                );
-              })}
-            </div>
-          </>
-        );
-      })}
     </Content>
   );
 };
