49538a852db89bd04463cc7f80f86cb1e402b779
Welles <3200102339@zju.edu.cn>
2023-08-26 00:28:37 +0800

    fix: add /api to backend service

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export default class Config {
-  static BASE_URL_DEV = "http://localhost:3000";
-  static BASE_URL_PRO = "https://www.copilot-m.top";
+  static BASE_URL_DEV = "http://localhost:3000/api";
+  static BASE_URL_PRO = "https://www.copilot-m.top/api";
   static BASE_URL = this.BASE_URL_PRO;
 }
